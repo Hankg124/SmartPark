b1f12ff7a58ee6984b519641cf71d87610e85270
Hankg124 <1416737291@qq.com>
2024-06-17 11:24:40 +0800

    init

---
+++ src/router/login/login.ts
@@ -0,0 +1,11 @@
+import request from '@/utils/request';
+
+
+// 登录
+export type loginParamsType={
+    password: string;
+    username: string;
+}
+export const loginApi=(loginParams:loginParamsType)=>{
+    return request('/park/login','POST',loginParams)
+}
---
+++ src/views/login/loginIndex.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+<button @click="loginFn">登录</button>
+</div>
+</template>
+<script lang='ts' setup>
+import { loginApi } from '@/router/login/login';
+
+const loginFn=async()=>{
+    await loginApi({username: "demo", password: "zh@hm#23"});
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
